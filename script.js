2237d2e60e573516216c34cd6faa95873a73081d
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-01-20 10:25:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// async function getData(){
+//     let apiResponse = await fetch(`https://gmail.googleapis.com/gmail/v1/users/visshnnutejaa@gmail.com/profile`)
+//     let apiData = await apiResponse.json()
+//     console.log(apiData)
+// }
+// getData()
